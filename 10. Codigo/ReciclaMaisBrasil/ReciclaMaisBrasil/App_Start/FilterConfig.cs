992da47ab2e12f5baf6304d09403062093998f8b
Caio Alves <caio_cersa_@hotmail.com>
2017-08-13 00:26:00 -0300

    Criação da Pasta Codigo e Criação das camadas.

---
+++ 10. Codigo/ReciclaMaisBrasil/ReciclaMaisBrasil/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ReciclaMaisBrasil
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
